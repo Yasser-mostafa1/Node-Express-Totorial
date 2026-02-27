15d5b480f24cd98174a9c35aa929b09636867644
Yasser-mostafa1 <mostafino4@gmail.com>
2026-02-27 15:07:49 +0200

    second commit

--- 11-fs-async.js
+++ 11-fs-async.js
@@ -1,24 +1,47 @@
-const {readFile, writeFile} = require ('fs');
+// First : Call the reqired methods from 'fs' module
+const { readFile, writeFile } = require('fs');
 
+    // 1- readFile function :
+ /*
+ - in this case we read 2 text files first.txt, second.txt located in the 'content' folder.
+ - syntax: readFile('./path', format, callback function)
+ */
+
+    // ~ first file read
 readFile(
-    './content/first.txt', 'utf8', (err, result) => {
+    './content/first.txt', 'utf8',
+    // this part calls : "callbacks" must be initialized with required function to handling any unexpected error.
+    // and to return a result if it's clear.
+     (err, result) => {
         if (err) {
             console.log(err);
             return
         }
         const first = result;
-         readFile(
+
+    // ~ second file read
+        readFile(
             './content/second.txt', 'utf8', (err, result) => {
                 if (err) {
                     console.log(err);
                     return
                 }
-                const second = result ;
+                const second = result;
 
+    // 2- writeFile function :
+    
+    /*
+    - createing new file by simply refering to the aimed path'./' and naming it by it's format.
+    - or overriding an existing one
+    - syntax: writeFile('./path', $ write, callback function)
+    */
                 writeFile(
+                    // creating new text file in content folder named result-async
                     './content/result-async.txt'
-                    , `Here is the result : ${first} , ${second}` 
-                    , (err,result) => {
+                    // we've previousely read first.txt, second.txt files so let's use them!
+                    // finally the benifit of assigned "result" in callback function of readFile method reaped its fruits!
+                    , `Here is the result : ${first} , ${second}`
+                    , (err, result) => {
                         if (err) {
                             console.log(err);
                             return;
@@ -27,6 +50,6 @@ readFile(
                     }
                 )
             }
-         )
+        )
     }
 )
